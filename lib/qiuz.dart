60204c1254fb7b29c8c25240a1148a1c6353da0a
Adenle Abiodun <adenleabbey@hotmail.com>
2021-05-21 11:32:45 +0100

    Added quiz widget

--- lib/qiuz.dart
+++ lib/qiuz.dart
@@ -1,30 +1,30 @@
-// import 'package:flutter/material.dart';
-// import 'question.dart';
-// import 'answer.dart';
+import 'package:flutter/material.dart';
+import 'question.dart';
+import 'answer.dart';
 
-// class Quiz extends StatelessWidget {
-//   final List<Map<String, Object>> questions;
-//   final Function answerQuestion;
-//   final int questionIndex;
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final Function answerQuestion;
+  final int questionIndex;
 
-//   Quiz(
-//     this.questions,
-//     this.answerQuestion,
-//     this.questionIndex,
-//   );
+  Quiz(
+    this.questions,
+    this.answerQuestion,
+    this.questionIndex,
+  );
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         Question(
-//           (questions[questionIndex]['questionText'] as String),
-//         ),
-//         ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
-//             .map((answer) {
-//           return Answer(() => answerQuestion(answer['score']), answer['text']);
-//         }).toList()
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          (questions[questionIndex]['questionText'] as String),
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
+        }).toList()
+      ],
+    );
+  }
+}
--- lib/result.dart
+++ lib/result.dart
@@ -6,7 +6,8 @@
 //   Result(this.resultScore, this.reset);
 
 //   String get resultPhrase {
-//     String resultText = "Yayy.... You did it, ";
+//     String resultText =
+//         "Yayy.... Your final score is " + resultScore.toString() + ". ";
 
 //     if (resultScore <= 8) {
 //       resultText += "You are awesome";
@@ -20,26 +21,26 @@
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         children: <Widget>[
-//           Text(
-//             resultPhrase,
-//             style: TextStyle(
-//                 fontSize: 30,
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.green[400]),
-//             textAlign: TextAlign.center,
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         Text(
+//           resultPhrase,
+//           style: TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.bold,
+//               color: Colors.black87),
+//           textAlign: TextAlign.center,
+//         ),
+//         TextButton(
+//           style: TextButton.styleFrom(
+//             textStyle: const TextStyle(fontSize: 25),
 //           ),
-//           ElevatedButton(
-//             style: ElevatedButton.styleFrom(
-//               primary: Colors.blue,
-//             ),
-//             child: Text("Restart Quiz"),
-//             onPressed: () => reset(),
-//           ),
-//         ],
-//       ),
+//           child: Text("Restart Quiz"),
+//           onPressed: () => reset(),
+//         ),
+//       ],
 //     );
 //   }
 // }
